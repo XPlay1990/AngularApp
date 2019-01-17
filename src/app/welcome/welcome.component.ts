d550829fd3016d5f6aa78599cc6549c0a629d5ed
Jan Adamczyk <j_adamczyk@hotmail.com>
2019-01-18 00:00:39 +0100

    preparations for linking with spring-boot

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeDataService } from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -9,10 +10,13 @@ import { ActivatedRoute } from '@angular/router';
 export class WelcomeComponent implements OnInit {
 
   private username=''
-  constructor(private route:ActivatedRoute) { }
+  constructor(private route:ActivatedRoute, private welcomeService:WelcomeDataService) { }
 
   ngOnInit() {
     this.username = this.route.snapshot.params['name']
   }
 
+  getWelcomeFromSpring(){
+    console.log(this.welcomeService.executeHelloWorldBeanService());
+  }
 }
